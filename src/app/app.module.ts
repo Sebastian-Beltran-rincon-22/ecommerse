01c90b5bfa9765dab93bb54d7ecf14b21cc6c7a0
sewastian <sebastianrincon834@gmail.com>
2024-02-19 11:29:21 -0500

    config -> tailwind created -> files pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AsideComponent } from './core/layout/aside/aside.component';
-import { FooterComponent } from './core/layout/footer/footer.component';
-import { HeaderComponent } from './core/layout/header/header.component';
-import { ModalComponent } from './core/libs/modal/modal.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AsideComponent,
-    FooterComponent,
-    HeaderComponent,
-    ModalComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
