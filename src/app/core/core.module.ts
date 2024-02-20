ca128fb84f817e42ccf42d0bfd57dd459c11f6ba
sewastian <sebastianrincon834@gmail.com>
2024-02-20 10:55:46 -0500

    creating and update [core -> aside, footer, header]

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './libs/modal/modal.component';
+import { AsideComponent } from './layout/aside/aside.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    AsideComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
