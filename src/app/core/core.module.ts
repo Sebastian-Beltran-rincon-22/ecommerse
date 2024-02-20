2781953c3edaa34169595313cad3744b279c42a6
sewastian <sebastianrincon834@gmail.com>
2024-02-20 09:48:48 -0500

    created -> component -> modal

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModalComponent } from './libs/modal/modal.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ModalComponent
+  ],
   imports: [
     CommonModule
   ]
