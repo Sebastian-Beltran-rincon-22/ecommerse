6885cb8cfcb79d17860a8d499b87fa6a02ede155
sewastian <sebastianrincon834@gmail.com>
2024-02-22 12:13:29 -0500

    feat [footer -> footer.component.ts]

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { JeanService } from '../../service/jean/jean.service';
+
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  currentIndex: number = 0;
+  jeanDataList: any;
+  visibleJeanDataList: any;
+  visibleImages: number = 3; // Ajusta según tu necesidad
 
+  constructor(private jeanService: JeanService) {}
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % (this.jeanDataList.length - this.visibleImages + 1);
+    this.updateVisibleJeanData();
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.jeanDataList.length) % (this.jeanDataList.length - this.visibleImages + 1);
+    this.updateVisibleJeanData();
+  }
+
+  updateVisibleJeanData() {
+    this.visibleJeanDataList = this.jeanDataList.slice(this.currentIndex, this.currentIndex + this.visibleImages);
+  }
+
+  ngOnInit(): void {
+    this.jeanService.getJeanData().subscribe(dataList => {
+      this.jeanDataList = dataList;
+      this.updateVisibleJeanData();
+    });
+  }
 }
+
