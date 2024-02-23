f2dbb020e111cc4209848b5cc43558f9ae2085cf
sewastian <sebastianrincon834@gmail.com>
2024-02-23 17:02:34 -0500

    update [app -> app.routing.module.ts]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PantsComponent } from './pages/pants/pants.component';
 import { ViewComponent } from './pages/view/view.component';
+import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
 
   {path:'',component: HomeComponent},
   {path:'catalogo/pant',component: PantsComponent},
-  {path:'view/rutadinamica',component: ViewComponent}
+  {path:'view/rutadinamica',component: ViewComponent},
+  {path:'login',component: LoginComponent},
+  {path:'register',component: RegisterComponent},
+
+
+  //pages not found
+  {path:'**',component: PageNotFoundComponent}
+
 ];
 
 @NgModule({
